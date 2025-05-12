e9e14a20b2105e6797c50b820957fca4b815bccf
Fanny Salomon <fsalomon@f6r9s2.clusters.42paris.fr>
2025-05-12 17:09:37 +0200

    debut de limplementation du lexer

---
+++ src/class/Lexer.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Lexer.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fsalomon <fsalomon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/12 17:07:10 by fsalomon          #+#    #+#             */
+/*   Updated: 2025/05/12 17:07:42 by fsalomon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "Lexer.hpp"
+
+
+Lexer::Lexer(){};
+
+Lexer::Lexer(const Lexer& src){ (void)src;}
+
+Lexer& Lexer::operator=(const Lexer& rhs) { (void)rhs; }
+
+Lexer::~Lexer(void) {}
+
---
+++ src/class/Lexer.hpp
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Lexer.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fsalomon <fsalomon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/12 17:07:17 by fsalomon          #+#    #+#             */
+/*   Updated: 2025/05/12 17:07:31 by fsalomon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LEXER_HPP
+# define LEXER_HPP
+
+#include <string>
+
+enum TokenType {
+    TOKEN_IDENTIFIER, //pour les mots comme server listen root
+    TOKEN_STRING, // pour les chemins , les noms (/var/www, localhost)
+    TOKEN_NUMBER, // pour des chiffres seules
+    TOKEN_COLON, // :
+    TOKEN_SEMICOLON, // ;
+    TOKEN_LBRACE, // {
+    TOKEN_RBRACE, // }
+    TOKEN_EOF, // fin du fichier
+    TOKEN_UNKNOWN // en cas derreur ou char inattendue
+};
+
+struct Token {
+    TokenType type; // type du token 
+    std::string value; // contenu exacte lu
+    int line; // ligne du fichier ou le token commence
+    int column; // colonne du debut du token
+};
+
+class Lexer {
+    public:
+    Lexer(const std::string &content);
+    Token nextToken(); // retourne le porchain token
+    bool hasMoreTokens() const; // retourne false quand EOF atteint 
+    ~Lexer();                           
+    
+    private :
+    Lexer();
+    Lexer(const Lexer& other);                         
+    Lexer& operator=(const Lexer& other);          
+    std::string _content; // contenu brut du fichier
+    size_t _pos; // position actuelle dans le texte
+    int _line; //ligne actuelle
+    int _column; //colonne actuelle
+
+    char peek() const; // regarde le prochain char sans avancer 
+    char get(); // avance et retourne le prochin char 
+    void skipWhitespace(); // Ignore les espaces et les sauts de ligne
+    void skipComment(); // Ignore les lignes de commentaire
+    Token parseIdentifier();
+    Token parseString();
+    Token parseSymbol();
+};
+
+#endif 
