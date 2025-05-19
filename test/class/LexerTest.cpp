34037250bc67d21a2fa751339701bfc4f4bd4375
Fanny Salomon <fsalomon@f6r12s16.clusters.42paris.fr>
2025-05-19 10:45:59 +0200

    add some tester to my lexzer

--- test/class/LexerTest.cpp
+++ test/class/LexerTest.cpp
@@ -6,7 +6,7 @@
 /*   By: fsalomon <fsalomon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 13:49:56 by fsalomon          #+#    #+#             */
-/*   Updated: 2025/05/13 15:03:47 by fsalomon         ###   ########.fr       */
+/*   Updated: 2025/05/19 10:45:35 by fsalomon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,6 +82,96 @@ TEST(TestLexer, parseString)
     Token t4 = object.nextToken();
     EXPECT_EQ(t4.type, TOKEN_EOF);
 }
+
+TEST(TestLexer, parsePathLike)
+{
+    std::string input = "location /images/icons;";
+    Lexer object(input);
+
+    Token t1 = object.nextToken();
+    EXPECT_EQ(t1.type, TOKEN_IDENTIFIER);
+    EXPECT_EQ(t1.value, "location");
+
+    Token t2 = object.nextToken();
+    EXPECT_EQ(t2.type, TOKEN_STRING);
+    EXPECT_EQ(t2.value, "/images/icons");
+
+    Token t3 = object.nextToken();
+    EXPECT_EQ(t3.type, TOKEN_SEMICOLON);
+    EXPECT_EQ(t3.value, ";");
+
+    Token t4 = object.nextToken();
+    EXPECT_EQ(t4.type, TOKEN_EOF);
+}
+
+TEST(TestLexer, parsePathLikeWithDashAndUnderscore)
+{
+    std::string input = "root /static/js/script-v1_2.js;";
+    Lexer object(input);
+
+    Token t1 = object.nextToken();
+    EXPECT_EQ(t1.type, TOKEN_IDENTIFIER);
+    EXPECT_EQ(t1.value, "root");
+
+    Token t2 = object.nextToken();
+    EXPECT_EQ(t2.type, TOKEN_STRING);
+    EXPECT_EQ(t2.value, "/static/js/script-v1_2.js");
+
+    Token t3 = object.nextToken();
+    EXPECT_EQ(t3.type, TOKEN_SEMICOLON);
+    EXPECT_EQ(t3.value, ";");
+
+    Token t4 = object.nextToken();
+    EXPECT_EQ(t4.type, TOKEN_EOF);
+}
+
+TEST(TestLexer, parsePathLikeWithDotPrefix)
+{
+    std::string input = "include ./config-site;";
+    Lexer object(input);
+
+    Token t1 = object.nextToken();
+    EXPECT_EQ(t1.type, TOKEN_IDENTIFIER);
+    EXPECT_EQ(t1.value, "include");
+
+    Token t2 = object.nextToken();
+    EXPECT_EQ(t2.type, TOKEN_STRING);
+    EXPECT_EQ(t2.value, "./config-site");
+
+    Token t3 = object.nextToken();
+    EXPECT_EQ(t3.type, TOKEN_SEMICOLON);
+    EXPECT_EQ(t3.value, ";");
+
+    Token t4 = object.nextToken();
+    EXPECT_EQ(t4.type, TOKEN_EOF);
+}
+
+TEST(TestLexer, parsePathLikeStopsAtInvalidCharacter)
+{
+    std::string input = "upload_store /uploads/images!;";
+    Lexer object(input);
+
+    Token t1 = object.nextToken();
+    EXPECT_EQ(t1.type, TOKEN_IDENTIFIER);
+    EXPECT_EQ(t1.value, "upload_store");
+
+    Token t2 = object.nextToken();
+    EXPECT_EQ(t2.type, TOKEN_STRING);
+    EXPECT_EQ(t2.value, "/uploads/images"); // le "!" ne fait pas partie du chemin
+
+    Token t3 = object.nextToken();
+    EXPECT_EQ(t3.type, TOKEN_UNKNOWN); // le "!" n'étant pas reconnu, à adapter selon ton implémentation
+    EXPECT_EQ(t3.value, "!");
+
+    Token t4 = object.nextToken();
+    EXPECT_EQ(t4.type, TOKEN_SEMICOLON);
+    EXPECT_EQ(t4.value, ";");
+
+    Token t5 = object.nextToken();
+    EXPECT_EQ(t5.type, TOKEN_EOF);
+
+}
+
 
 TEST(TestLexer, ErrorToken)
 {
