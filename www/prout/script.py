0931ab4fecc3734de9b027b47898d99a42002400
Antoine Astruc <ajourdanastruc@gmail.com>
2025-06-12 12:07:06 +0200

    post cgi ok

--- www/prout/script.py
+++ www/prout/script.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
-# print("Content-Type: text/html")
-# print()
-# print("<h1>Hello from CGI!</h1>")
+import os
+import sys
+
+print("Content-Type: text/html")
+print()
+print("<h1>Hello from CGI!</h1>")
+
+# Récupérer la longueur du body (POST)
+content_length = os.environ.get("CONTENT_LENGTH")
+if content_length and content_length.isdigit() and int(content_length) > 0:
+    body = sys.stdin.read(int(content_length))
+    print("<pre>")
+    print(body)
+    print("</pre>")
 
 # while True:
 #     pass  # boucle infinie pour tester le timeout du serveur
-
